baad99537a0cf7887a6484b36c1f131b05dcb861
ZoeCheck <893622396@qq.com>
2014-01-15 13:50:23 +0800

    Add project

---
+++ MP3BreakpointParser/CaseInsensitiveComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MP3BreakpointParser
+{
+    public class CaseInsensitiveComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
